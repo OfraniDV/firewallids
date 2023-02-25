d94e70813191b1e30f015494f474be243da99345
Ofrani Diaz Vizcay <121466972+OfraniDV@users.noreply.github.com>
2023-02-25 09:37:21 -0800

    Consultando en 2 tablas para verificar si ya tiene KYC realizado

--- app.js
+++ app.js
@@ -98,23 +98,35 @@ bot.action('kyc', async (ctx) => {
   // Obtiene el ID del usuario
   const userId = ctx.from.id;
 
-  // Consulta si el ID del usuario está en la tabla kycfirewallids
   try {
-    const result = await pool.query('SELECT * FROM kycfirewallids WHERE user_id = $1', [userId]);
+    // Consulta si el usuario ya completó el KYC en la tabla kycfirewallids
+    const kycResult = await pool.query('SELECT * FROM kycfirewallids WHERE user_id = $1', [userId]);
 
-    if (result.rows.length === 0) {
-      // Si el usuario no está en la tabla, borra el mensaje actual y muestra el nuevo menú
-      await ctx.deleteMessage();
-      await menuKYC.mostrarMenu(ctx);
-    } else {
-      // Si el usuario ya está en la tabla, envía un mensaje indicando que ya completó el KYC
+    if (kycResult.rows.length > 0) {
+      // Si el usuario ya completó el KYC, envía un mensaje indicando que ya completó el proceso
       await ctx.answerCbQuery('Ya has completado el proceso KYC');
+      return;
     }
+
+    // Consulta si el usuario ya completó el KYC en la tabla identidades
+    const idResult = await pool.query('SELECT * FROM identidades WHERE usuario_id = $1', [userId]);
+
+    if (idResult.rows.length > 0) {
+      // Si el usuario ya completó el KYC, envía un mensaje indicando que ya completó el proceso
+      await ctx.answerCbQuery('Ya has completado el proceso KYC');
+      return;
+    }
+
+    // Si el usuario no ha completado el KYC, borra el mensaje actual y muestra el menú KYC
+    await ctx.deleteMessage();
+    await menuKYC.mostrarMenu(ctx);
+
   } catch (err) {
     console.log(err);
     await ctx.answerCbQuery('Ha ocurrido un error al procesar su solicitud');
   }
 });
+
 
 //  BOTONES DE LOS MENUS DEL KYC
 // menu kyc
