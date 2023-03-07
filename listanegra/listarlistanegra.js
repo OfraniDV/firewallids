daed1197dfdba7a12e29adf686611f677c2a41aa
Ofrani Diaz Vizcay <121466972+OfraniDV@users.noreply.github.com>
2023-03-07 14:59:08 -0800

    listados

--- listanegra/listarlistanegra.js
+++ listanegra/listarlistanegra.js
@@ -1,65 +1,49 @@
 const { pool } = require('../psql/db');
 const moment = require('moment-timezone');
-const excel = require('exceljs');
+const path = require('path');
+const { Telegraf } = require('telegraf');
 const fs = require('fs');
-
+const ExcelJS = require('exceljs');
 
 const formatTable = (rows) => {
-  const header = ['Fila', 'ID', 'Motivo', 'Tiempo'].map((value) => ({ name: value }));
+  const header = ['Fila', 'ID', 'Motivo', 'Fecha'].map((val) => ({ header: val, key: val.toLowerCase() }));
 
-  const body = rows.map((row, index) => {
-    return { fila: index + 1, id: row.id, motivo: row.motivos, tiempo: row.tiempo };
+  const data = rows.map((row, index) => {
+    return {
+      fila: index + 1,
+      id: row.id,
+      motivo: row.motivos,
+      fecha: moment.tz(row.tiempo, 'America/Havana').format('DD/MM/YYYY hh:mm A'),
+    };
   });
 
-  return [header, ...body];
+  return { header, data };
 };
 
 const listarUsuariosEnListaNegra = async (ctx) => {
   try {
-    const result = await pool.query(
-      `SELECT id, motivos, tiempo FROM listanegra ORDER BY tiempo ASC`
-    );
-
-    const workbook = new excel.Workbook();
-    const sheet = workbook.addWorksheet('Usuarios en lista negra');
-
-    const headerRow = sheet.addRow([
-      'Estos usuarios se encuentran en la lista negra del sistema.',
-      `Total de usuarios en lista negra: ${result.rows.length}`,
-    ]);
-    headerRow.font = { bold: true };
-    sheet.addRow([]);
+    const result = await pool.query(`SELECT id, motivos, tiempo FROM listanegra ORDER BY tiempo ASC`);
+    const countResult = await pool.query(`SELECT COUNT(*) FROM listanegra`);
+    const totalUsuariosEnListaNegra = countResult.rows[0].count;
 
     const currentDateTime = moment.tz('America/Havana').format('DD/MM/YYYY hh:mm A');
-    const subtitleRow = sheet.addRow([`Fecha y hora de generación del archivo: ${currentDateTime}`]);
-    subtitleRow.font = { italic: true };
-    sheet.addRow([]);
+    const filename = `lista_negra_${currentDateTime.replace(/[\W_]/g, '_')}.xlsx`;
+    const filepath = path.join(__dirname, '..', filename);
 
     const table = formatTable(result.rows);
-    table.forEach((row) => {
-      sheet.addRow(row);
-    });
 
-    sheet.columns.forEach((column) => {
-      let maxLength = 0;
-      column.eachCell({ includeEmpty: true }, (cell) => {
-        const columnLength = cell.value ? cell.value.toString().length : 10;
-        if (columnLength > maxLength) {
-          maxLength = columnLength;
-        }
-      });
-      column.width = maxLength < 10 ? 10 : maxLength;
-    });
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('Lista Negra');
+    worksheet.columns = table.header;
+    worksheet.addRows(table.data);
 
-    const filename = `lista_negra_${currentDateTime.replace(/[\W_]/g, '_')}.xlsx`;
+    await workbook.xlsx.writeFile(filepath);
 
-    await workbook.xlsx.writeFile(filename);
+    const bot = new Telegraf(process.env.BOT_TOKEN);
+    const caption = `Esta es la lista de usuarios en lista negra generada el ${currentDateTime} en el Sistema de Reputación Plus y Firewallids. Total de usuarios en lista negra: ${totalUsuariosEnListaNegra}`;
+    await ctx.replyWithDocument({ source: filepath }, { caption });
 
-    const file = await fs.promises.readFile(filename);
-
-    await ctx.replyWithDocument({ filename, source: file }, { caption: `Estos son los usuarios que se encuentran en lista negra en el Sistema de Reputación Plus y Firewallids.` });
-
-    await fs.promises.unlink(filename);
+    fs.unlinkSync(filepath);
   } catch (error) {
     console.error(`Error al ejecutar el comando /listanegra: ${error}`);
     await ctx.reply('Ocurrió un error al ejecutar el comando');
@@ -69,3 +53,4 @@ const listarUsuariosEnListaNegra = async (ctx) => {
 module.exports = {
   listarUsuariosEnListaNegra,
 };
+
