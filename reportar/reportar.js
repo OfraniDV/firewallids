026c1b3d9071dad061456ae5dc6ed1218944606c
Ofrani Diaz Vizcay <121466972+OfraniDV@users.noreply.github.com>
2023-03-09 08:21:21 -0800

    reportes de los usuarios

--- reportar/reportar.js
+++ reportar/reportar.js
@@ -10,7 +10,23 @@ const bot = new Telegraf(process.env.BOT_TOKEN, { allow_callback_query: true });
 
 async function reportar(ctx) {
   const userId = ctx.from.id;
-  const mensajeReporte = `Reporte de usuario: ${ctx.from.first_name} (${userId})\n\nMensaje: ${ctx.message.text.split(' ').slice(1).join(' ')}`;
+  const chatId = ctx.chat.id;
+  const chatTitle = ctx.chat.title;
+  const chatType = ctx.chat.type;
+  let reporte = ctx.message.text.split(' ').slice(1).join(' ');
+
+  let mensajeReporte = '';
+  let origenMensaje = '';
+  if (chatType === 'group' || chatType === 'supergroup') {
+    mensajeReporte = `Reporte de usuario: ${ctx.from.first_name} (${userId})\n\nMensaje enviado desde el grupo "${chatTitle}" (${chatId}):\n${reporte}\n\n`;
+    origenMensaje = `Este mensaje fue enviado desde el grupo "${chatTitle}" (${chatId}).`;
+  } else if (chatType === 'private') {
+    mensajeReporte = `Reporte de usuario: ${ctx.from.first_name} (${userId})\n\nMensaje enviado desde el chat privado del bot:\n${reporte}\n\n`;
+    origenMensaje = `Este mensaje fue enviado desde un chat privado con el bot.`;
+  }
+
+  mensajeReporte += origenMensaje;
+
   const client = await pool.connect();
   try {
     const res = await client.query('INSERT INTO reportes (ticket, user_id, reporte) VALUES (nextval(\'reportes_ticket_seq\'), $1, $2) RETURNING ticket', [userId, mensajeReporte]);
@@ -19,7 +35,7 @@ async function reportar(ctx) {
     console.log(`Nuevo reporte recibido. El número de ticket es: ${ticket}`);
     await ctx.replyWithMarkdown(`¡Tu reporte se ha enviado a los administradores! Tu número de ticket es: \`${ticket}\` 🎫`);
 
-    const mensajeAdmin = `🔔 Nuevo reporte recibido. El número de ticket es: ${ticket}\n\n📢 *Reporte de usuario* 📢\n\n👤 Usuario: ${ctx.from.first_name} (${userId})\n\n📩 Mensaje: ${escape(mensajeReporte)}\n\n`;
+    const mensajeAdmin = `🔔 Nuevo reporte recibido. El número de ticket es: ${ticket}\n📢 *Reporte de usuario* 📢\n👤 Usuario: ${ctx.from.first_name} (${userId})\n\n📩 Mensaje: ${escape(reporte)}\n\n${origenMensaje}`;
     const adminList = [process.env.ID_GROUP_ADMIN];
     for (let admin of adminList) {
       try {
