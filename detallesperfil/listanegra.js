4a793d6799b898ac2e2c889dcc540c693a08dc5e
Ofrani Diaz Vizcay <121466972+OfraniDV@users.noreply.github.com>
2023-03-09 08:45:09 -0800

    reportes de los usuarios

--- detallesperfil/listanegra.js
+++ detallesperfil/listanegra.js
@@ -6,7 +6,7 @@ async function checkListanegra(userID) {
     const result = await client.query('SELECT * FROM listanegra WHERE id = $1', [userID]);
     if (result.rows.length > 0) {
       const motivo = result.rows[0].motivos;
-      return `Este usuario está en la Lista Negra por el siguiente motivo: ${motivo}`;
+      return `\n${motivo}`;
     } else {
       return null;
     }
