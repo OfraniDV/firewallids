07e95e7fba7648eb6448ee9823c56a5d53c57960
Ofrani Diaz Vizcay <121466972+OfraniDV@users.noreply.github.com>
2023-03-08 16:15:25 -0800

    comando cambios

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const fs = require('fs');
 const path = require('path');
 
 
-
-
 const { md, escapeMarkdown } = require('telegram-escape')
 
 // Importar comandos
@@ -54,7 +52,6 @@ const { cleanGroups } = require('./listanegra/clean');
 
 
 
-
 //Conexion del BOT Variables de Entorno
 const bot = new Telegraf(process.env.BOT_TOKEN, { allow_callback_query: true });
 const ID_GROUP_VERIFY_KYC = process.env.ID_GROUP_VERIFY_KYC;
@@ -74,7 +71,7 @@ const rules  = process.env.BOT_RULES;
 
 ////    **** LISTA NEGRA ****              ////
 // Listar los usuarios en lista negra
-/*bot.command('lslistanegra', async (ctx) => {
+bot.command('lslistanegra', async (ctx) => {
   try {
     const adminId = ctx.from.id;
     const isAdmin = await pool.query(`SELECT id FROM listanegra_administradores WHERE id = ${adminId}`);
@@ -112,308 +109,12 @@ bot.command('clean', async (ctx) => {
     // Llamamos a la función cleanGroups para realizar la limpieza
     await cleanGroups(ctx);
   }, 1000);
-});*/
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////
-// ****************          ****  // KYC //  *****       ************
-// Actualizar el KYC de la Tabla Firewallids a Identidades para BRPlus////////////
-const updateIdentidad = async () => {
-  try {
-    const query = `
-      UPDATE identidades
-      SET documento_identidad_numero = kycfirewallids.identity_number
-      FROM kycfirewallids
-      WHERE identidades.usuario_id = kycfirewallids.user_id;
-    `;
-    await pool.query(query);
-    console.log('Update de documento de identidad completado');
-  } catch (error) {
-    console.error('Error al actualizar documento de identidad:', error);
-  }
-};
-
-module.exports = { updateIdentidad };
-
-
-
-
-
-const updateProvincia = async () => {
-  try {
-    const query = `UPDATE identidades SET direccion_region = kycfirewallids.province, direccion_codigo_pais = 'CU' 
-                    FROM kycfirewallids WHERE kycfirewallids.user_id = identidades.usuario_id`;
-    const result = await pool.query(query);
-    console.log(`Se actualizaron ${result.rowCount} filas en la tabla identidades en la columna direccion_region y direccion_codigo_pais.`);
-  } catch (error) {
-    console.error('Error en updateProvincia: ', error);
-  }
-}
-
-module.exports = { updateProvincia };
-
-
-
-const updateMunicipio = async () => {
-  try {
-    const client = await pool.connect();
-    const query = `
-      UPDATE identidades
-      SET direccion_localidad = kycfirewallids.municipality
-      FROM kycfirewallids
-      WHERE identidades.usuario_id = kycfirewallids.user_id
-    `;
-    await client.query(query);
-    console.log('Municipio actualizado en identidades');
-    client.release();
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-module.exports = { updateMunicipio };
-
-async function updateNameFull() {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT kycfirewallids.user_id, kycfirewallids.name FROM kycfirewallids WHERE kycfirewallids.approved = true');
-    const users = result.rows;
-    
-    for (let i = 0; i < users.length; i++) {
-      const user = users[i];
-      const name = user.name.trim().split(' ');
-
-      if (name.length === 4) {
-        const firstName = name[0];
-        const secondName = name[1];
-        const lastName = `${name[2]} ${name[3]}`;
-
-        await client.query('UPDATE identidades SET datos_personales_primer_nombre = $1, datos_personales_segundo_nombre = $2, datos_personales_apellidos = $3 WHERE usuario_id = $4', [firstName, secondName, lastName, user.user_id]);
-      } else if (name.length === 3) {
-        const firstName = name[0];
-        const lastName = `${name[1]} ${name[2]}`;
-
-        await client.query('UPDATE identidades SET datos_personales_primer_nombre = $1, datos_personales_apellidos = $2 WHERE usuario_id = $3', [firstName, lastName, user.user_id]);
-      }
-    }
-
-    console.log('updateNameFull executed successfully');
-    client.release();
-  } catch (error) {
-    console.error('Error executing updateNameFull: ', error);
-  }
-}
-
-module.exports = { updateNameFull };
-
-
-const updateEmail = async () => {
-  try {
-    const result = await pool.query(
-      `SELECT kyc.user_id, kyc.email, identidades.id 
-      FROM kycfirewallids AS kyc
-      INNER JOIN identidades ON kyc.user_id = identidades.usuario_id
-      WHERE kyc.approved = true AND identidades.estado = 1`
-    );
-
-    console.log(`Actualizando ${result.rows.length} correos electrónicos en la tabla identidades...`);
-
-    for (const user of result.rows) {
-      const email = user.email;
-      const identidades_id = user.id;
-
-      await pool.query(
-        `UPDATE identidades SET correo = $1 WHERE id = $2`,
-        [email, identidades_id]
-      );
-
-      console.log(` - Actualizado correo en la fila de la tabla identidades con id ${identidades_id}`);
-    }
-
-    console.log('Actualización de correos electrónicos completada');
-  } catch (err) {
-    console.error(`Error al actualizar correos electrónicos en la tabla identidades: ${err}`);
-  }
-};
-
-module.exports = { updateEmail };
-
-
-const updatePhone = async () => {
-  try {
-    const result = await pool.query(
-      `SELECT kyc.user_id, kyc.phone_number, identidades.id
-       FROM kycfirewallids kyc
-       JOIN identidades ON kyc.user_id = identidades.usuario_id
-       WHERE kyc.approved = true AND identidades.estado = 1`
-    );
-
-    console.log(`Actualizando ${result.rows.length} telefonos en la tabla identidades...`);
-
-    for (const row of result.rows) {
-      const { user_id, phone_number, id } = row;
-
-      console.log(`- Actualizando el telefono del usuario con user_id ${user_id}`);
-
-      const identidades = await pool.query(
-        `UPDATE identidades SET telefono = $1 WHERE id = $2 RETURNING *`,
-        [phone_number, id]
-      );
-    }
-
-    console.log('Telefonos actualizados correctamente en la tabla identidades');
-  } catch (err) {
-    console.error(`Error al actualizar telefonos en la tabla identidades: ${err}`);
-  }
-};
-
-module.exports = { updatePhone };
-
-
-const updateName = async () => {
-  try {
-    const result = await pool.query(
-      `SELECT * FROM kycfirewallids WHERE approved = true`
-    );
-
-    console.log(`Actualizando usuario_nombre para ${result.rows.length} usuarios...`);
-
-    for (const user of result.rows) {
-      const response = await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/getChatMember?chat_id=${user.user_id}&user_id=${user.user_id}`);
-      const json = await response.json();
-      const telegramUser = json.result.user;
-      const firstname = telegramUser.first_name;
-
-      console.log(` - Actualizando usuario_nombre en la tabla identidades para el usuario con user_id ${user.user_id}: ${firstname}`);
-
-      const identidades = await pool.query(
-        `UPDATE identidades SET usuario_nombre = $1 WHERE usuario_id = $2 AND estado = 1 RETURNING *`,
-        [firstname, user.user_id]
-      );
-    }
-
-    console.log('Todos los usuario_nombre han sido actualizados correctamente');
-  } catch (err) {
-    console.error(`Error al actualizar usuario_nombre: ${err}`);
-  }
-};
-
-module.exports = { updateName };
-
-
-const updateAlias = async () => {
-  try {
-    const result = await pool.query(
-      `SELECT * FROM kycfirewallids WHERE approved = true`
-    );
-
-    console.log(`Actualizando ${result.rows.length} aliases de Telegram...`);
-
-    for (const user of result.rows) {
-      const telegramUser = await getTelegramUser(user.user_id);
-      const alias = telegramUser ? telegramUser.alias : null;
-      
-      if(alias){
-        await pool.query(
-          `UPDATE identidades SET usuario_usuario = $1 WHERE usuario_id = $2 AND estado = 1`,
-          [alias, user.user_id]
-        );
-      }
-    }
-
-    console.log('Aliases de Telegram actualizados correctamente');
-  } catch (err) {
-    console.error(`Error al actualizar aliases de Telegram: ${err}`);
-  }
-};
-
-const getTelegramUser = async (telegramId) => {
-  try {
-    const response = await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/getChatMember?chat_id=${telegramId}&user_id=${telegramId}`);
-    const json = await response.json();
-    const user = json.result.user;
-    return {
-      firstname: user.first_name,
-      alias: user.username,
-    };
-  } catch (error) {
-    console.error(`Error al obtener el usuario de Telegram con ID ${telegramId}: ${error}`);
-    return null;
-  }
-};
-
-module.exports = { updateAlias };
-
-
-const insertUserIdentidades = async () => {
-  try {
-    const result = await pool.query(
-      `SELECT * FROM kycfirewallids WHERE approved = true`
-    );
-    console.log(`Encontrados ${result.rows.length} usuarios a insertar en la tabla identidades`);
-
-    for (const user of result.rows) {
-      const identidadesResult = await pool.query(
-        `SELECT * FROM identidades WHERE usuario_id = $1`,
-        [user.user_id]
-      );
-
-      if (identidadesResult.rows.length === 0) {
-        console.log(`Insertando nueva fila en la tabla identidades para el usuario_id: ${user.user_id}`);
-        await pool.query(
-          `INSERT INTO identidades (usuario_id, estado) VALUES ($1, 1)`,
-          [user.user_id]
-        );
-      }
-    }
-    console.log(`Inserción de usuarios completada`);
-  } catch (err) {
-    console.error(`Error al insertar usuarios en la tabla identidades: ${err}`);
-  }
-};
-
-module.exports = {
-  insertUserIdentidades,
-};
-
-//////////////////////////////// Funciones para poder actualizar identidades/////
-// EL COMANDO ///
-/////////////////////////////////////////////////////////////////////////////////
-/*bot.command('updatekyc', async (ctx) => {
-  try {
-    // Ejecutar las funciones una por una
-    await insertUserIdentidades();
-    await updateIdentidad();
-    await updateProvincia();
-    await updateMunicipio();
-    await updateNameFull();
-    await updateEmail();
-    await updatePhone();
-    await updateName();
-    await updateAlias();
-
-    // Enviar mensaje de éxito
-    await ctx.reply('Se han actualizado los datos de KYC correctamente.');
-        
-  } catch (error) {
-    console.error(error);
-    await ctx.reply('Ha ocurrido un error al actualizar los datos de KYC.');
-  }
 });
-*/
 
 
 
 
-
-
-
+// ***************************************************************//
 // Actualizar el KYC de la Tabla Firewallids a Identidades para BRPlus funcionando
 bot.command('updateusers', async (ctx) => {
   try {
@@ -428,11 +129,6 @@ bot.command('updateusers', async (ctx) => {
     await ctx.reply('🚫 Ha ocurrido un error al actualizar los usuarios.');
   }
 });
-
-
-
-
-
 
 
 //Botón KYC del Primer menu de COMANDOS PARA USUARIOS
@@ -508,7 +204,7 @@ bot.action('cancelKYC', async (ctx) => {
 ///////////////////////////////////////////////////////////////////////////////////
 
 // Listar Verificados
-/*bot.command('lsverificados', async (ctx) => {
+bot.command('lsverificados', async (ctx) => {
   try {
     const adminId = ctx.from.id;
     const isAdmin = await pool.query(`SELECT id FROM listanegra_administradores WHERE id = ${adminId}`);
@@ -524,11 +220,69 @@ bot.action('cancelKYC', async (ctx) => {
     console.error(`Error al ejecutar el comando /lsverificados: ${error}`);
     await ctx.reply('Ocurrió un error al ejecutar el comando');
   }
-});*/
+});
 
 
 
 
+//          ********** NOTIFICAR AL USUARIO ***********
+
+bot.command('notificar', async (ctx) => {
+  const args = ctx.message.text.split(' ');
+  
+  // Verificar si se proporcionó un ID
+  if (!args[1]) {
+    ctx.reply('⚠️ Es necesario especificar un ID para enviar el mensaje.');
+    return;
+  }
+
+  const id = parseInt(args[1]);
+  const mensaje = args.slice(2).join(' ');
+  
+  // Verificar si el usuario que ejecuta el comando es un administrador
+  const { rows: adminRows } = await pool.query(`
+    SELECT * FROM listanegra_administradores WHERE id = $1;
+  `, [ctx.from.id]);
+
+  if (adminRows.length === 0) {
+    ctx.reply('🚫 Acceso denegado. Solo los administradores pueden usar este comando.');
+    return;
+  }
+
+  // Verificar si el ID es válido
+  if (isNaN(id)) {
+    ctx.reply('⚠️ El ID proporcionado no es válido.');
+    return;
+  }
+
+  // Verificar si el usuario existe en Telegram
+  const telegramUser = await ctx.telegram.getChat(id).catch(() => null);
+  if (!telegramUser) {
+    ctx.reply('⚠️ El usuario con el ID proporcionado no existe en Telegram.');
+    return;
+  }
+
+  const adminAlias = ctx.from.username ? `@${ctx.from.username}` : ctx.from.first_name;
+
+  let mensajeFinal = `🥷¡SALUDOS!🥷\n\n Soy un *Administrador* del Bot donde solicitas que te aprueben tu KYC y necesito que entres en contacto conmigo para hacer unas comprobaciones referentes a tu solicitud de revisiones del KYC. Para ser aprobado, es necesario que nos contactes.
+  
+  Administrador: ${adminAlias}`;
+
+  if (mensaje) {
+    mensajeFinal += `\n\n*📩 Mensaje:* ${mensaje}`;
+  }
+
+  ctx.telegram.sendMessage(id, mensajeFinal, { parse_mode: "Markdown" });
+  ctx.reply('✅ Mensaje enviado.');
+});
+
+
+
+
+
+
+
+//****************************************************************************/
 
 //                                               Nombre Completo
 // Acción para pedir el nombre completo del usuario
@@ -1173,7 +927,7 @@ bot.action('kyc_approval', async (ctx) => {
       `Municipio: ${user.municipality}\n` +
       `Provincia: ${user.province}\n` +
       `Facebook: ${user.facebook}\n\n` +
-      `Para aprobar este KYC, ejecute el comando /aprobarkyc <ID_USUARIO>. Para rechazarlo, use el comando /rechazarkyc <ID_USUARIO>.`;
+      `Para aprobar este KYC, ejecute el comando /aprobarkyc <ID_USUARIO>.\n Para rechazarlo, use el comando /rechazarkyc <ID_USUARIO>.\n Si necesitas que el usuario aporte algo mas a tu criterio para poder aprobar este KYC escribe el comando /notificar <ID del Usuario> Texto al usuario.`;
 
     const kycFileSent = await ctx.telegram.sendDocument(process.env.ID_GROUP_VERIFY_KYC, { source: kycFileName }, { caption: reportMsg });
 
@@ -1188,7 +942,7 @@ bot.action('kyc_approval', async (ctx) => {
     `;
     await pool.query(updateQuery);
 
-    ctx.reply("¡Su solicitud de verificación de KYC ha sido enviada al departamento de revisiones para su revisión! Por favor, espere a que su solicitud sea revisada. Si tiene alguna pregunta, por favor, póngase en contacto con el grupo de soporte técnico.");
+    ctx.reply("¡Su solicitud de verificación de KYC ha sido enviada al departamento de revisiones para su revisión!\nPor favor, espere a que su solicitud sea revisada en un plazo de 48 horas.\nSi tiene alguna pregunta, por favor, póngase en contacto con el grupo de soporte técnico.");
   } catch (error) {
     console.error(error);
     ctx.reply(`Lo siento, ha ocurrido un error al enviar su solicitud de verificación de KYC. Por favor, inténtelo de nuevo más tarde o póngase en contacto con el grupo de soporte técnico.`);
