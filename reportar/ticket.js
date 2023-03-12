9f4f7e9366c56db1f4576b9dc15e3950af4ed540
Ofrani Diaz Vizcay <121466972+OfraniDV@users.noreply.github.com>
2023-03-12 10:18:26 -0700

    bugs de tickets

--- reportar/ticket.js
+++ reportar/ticket.js
@@ -1,11 +1,30 @@
-const { md, escapeMarkdown } = require('telegram-escape');
 const { pool } = require('../psql/db');
 const { findIdByAlias } = require('../commands/alias');
+const { md, escapeMarkdown } = require('telegram-escape');
 
 async function mostrarTicket(ctx) {
   const ticket = ctx.message.text.split(' ')[1];
   if (!ticket) {
-    // código omitido
+    const client = await pool.connect();
+    try {
+      const res = await client.query('SELECT ticket, user_id, fecha FROM reportes WHERE resolver = false OR resolver IS NULL');
+      if (res.rowCount === 0) {
+        return await ctx.reply(`No tengo tickets pendientes por resolver. ¡Buen trabajo! 👍`);
+      }
+      let mensaje = `📌 *Tickets pendientes* 📌\n\n`;
+      let numTickets = 0;
+      for (let row of res.rows) {
+        numTickets++;
+        const fechaFormateada = new Date(row.fecha).toLocaleDateString('es-ES', { day: '2-digit', month: '2-digit', year: '2-digit' });
+        mensaje += `🎫 Ticket #${row.ticket} (${fechaFormateada}) pendiente del usuario ${row.user_id}\n`;
+      }
+      mensaje += `\nEn total existen ${numTickets} tickets pendientes de resolución.`;
+      return await ctx.replyWithMarkdown(mensaje);
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      client.release();
+    }
   }
   const client = await pool.connect();
   try {
@@ -14,13 +33,10 @@ async function mostrarTicket(ctx) {
       return await ctx.reply(`Lo siento, no se encontró ningún reporte con el número de ticket ${ticket}.`);
     }
     const { user_id, reporte, fecha, resolver, solucion, id_admin_resolvio } = res.rows[0];
-
     let userName = await findIdByAlias(user_id);
     if (!userName) {
-      // En caso de no encontrar el alias, se muestra el ID
       userName = user_id;
     }
-
     let mensaje = `🎫 *Ticket #${ticket}* 🎫\n\n`;
     mensaje += `📆 Fecha del reporte: ${new Date(fecha).toLocaleDateString('es-ES', { day: '2-digit', month: '2-digit', year: '2-digit' })}\n\n`;
     mensaje += `👤 Usuario que envió el reporte: ${userName}\n\n`;
@@ -30,11 +46,12 @@ async function mostrarTicket(ctx) {
     } else {
       mensaje += `❌ Este reporte aún no ha sido resuelto.`;
     }
-    return await ctx.replyWithMarkdown(mensaje);
+    await ctx.replyWithMarkdown(mensaje);
   } catch (err) {
     console.log(err.stack);
   } finally {
     client.release();
   }
 }
+
 module.exports = { mostrarTicket };
